7ed4e080efda87bdab9678af24a264f370ea917d
abhishekchh <abhishek.chhetri@outlook.com>
2019-07-15 16:02:54 +0530

    Create test_script.py

---
+++ test_script.py
@@ -0,0 +1,35 @@
+# importing csv module 
+import csv 
+import os
+
+# csv file name 
+filename = "list.csv"
+
+# initializing the titles and rows list 
+fields = [] 
+rows = [] 
+
+# reading csv file 
+with open(filename, 'r') as csvfile: 
+	# creating a csv reader object 
+	csvreader = csv.reader(csvfile) 	
+	# extracting field names through first row 
+	fields = csvreader.next() 
+	# extracting each data row one by one 
+	for row in csvreader: 
+		rows.append(row) 
+	# get total number of rows 
+	print("Total no. of rows: %d"%(csvreader.line_num)) 
+
+# printing the field names 
+print('Field names are:' + ', '.join(field for field in fields)) 
+
+print('executing tests') 
+for row in rows[:5]:
+	if(row[1]=='y'):
+		#executing commands
+		x=os.system(row[0])
+		print("status of print is %d", x)
+	print('\n') 
+
+print("end of script")
