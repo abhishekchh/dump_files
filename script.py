c4df0612e6f1bac855224ca7b317564e23848f49
abhishekchh <abhishek.chhetri@outlook.com>
2019-07-15 10:16:30 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,34 @@
-print("Hello world")
+# importing csv module 
+import csv 
+import os
+
+# csv file name 
+filename = "list.csv"
+
+# initializing the titles and rows list 
+fields = [] 
+rows = [] 
+
+# reading csv file 
+with open(filename, 'r') as csvfile: 
+	# creating a csv reader object 
+	csvreader = csv.reader(csvfile) 	
+	# extracting field names through first row 
+	fields = csvreader.next() 
+	# extracting each data row one by one 
+	for row in csvreader: 
+		rows.append(row) 
+	# get total number of rows 
+	print("Total no. of rows: %d"%(csvreader.line_num)) 
+
+# printing the field names 
+print('Field names are:' + ', '.join(field for field in fields)) 
+
+print('\nFirst 5 rows are:\n') 
+for row in rows[:5]:
+	if(row[1]=='y'):
+		#executing commands
+		os.system(row[0])
+	print('\n') 
+
+print("end of script")
